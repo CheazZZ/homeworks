62e64bbf6c5b8336e50fd0a6f0b61c8c5e1a04aa
Shoxrux Yuldashov <deagleshox@mail.ru>
2023-12-17 18:00:36 +0000

    Абстрактный класс
    - Абстрактный класс и базовый класс животных
    - Реализованы классы `Cat`, `Dog`

--- main.py
+++ main.py
@@ -1,49 +1,101 @@
-class Animal:
+import abc
 
-    def __init__(self, name, age, sex, date_of_birth):
+
+class Animal(abc.ABC):
+    """Абстрактный класс."""
+
+    @abc.abstractmethod
+    def run(self):
+        pass
+
+    @abc.abstractmethod
+    def scratch(self):
+        pass
+
+    @abc.abstractmethod
+    def voice(self):
+        pass
+
+    @abc.abstractmethod
+    def jump(self):
+        pass
+
+
+class AnimalBase(Animal):
+    """Базовый класс для животных."""
+
+    def __init__(self, name, sex, date_of_birth):
         self.name = name
-        self.age = age
         self.sex = sex
         self.date_of_birth = date_of_birth
 
     def run(self):
-        print("I can run faster, than you!")
+        print('I can run faster, than you!')
 
     def scratch(self):
-        print('scratching')
+        print('Scratching')
 
     def voice(self):
-        print("there is some voice of animal here")
+        print('There is some voice of animal here')
 
     def jump(self):
-        return 'Jump!'
+        print('Jump!')
+
+    def __get_was_born(self):
+        if self.sex == 'мужской':
+            return 'родился'
+        elif self.sex == 'женский':
+            return 'родилась'
 
     def get_info(self):
-        FEMALE = 'женский', 'Женский'
-
-        if self.sex == FEMALE:
-            print(f"Привет!\n"
-                  f"Моя кличка: {self.name}!\n"
-                  f"Мой пол: {self.sex}\n"
-                  f"Мне {self.age} лет.\n" 
-                  f"Я родилась {self.date_of_birth}г.\n")
-        else:
-            print(f"Привет!\n"
-                  f"Моя кличка: {self.name}!\n"
-                  f"Мой пол: {self.sex}\n"
-                  f"Мне {self.age} лет.\n"
-                  f"Я родился {self.date_of_birth}г.\n")
+        template: str = ('\nПривет!'
+                         f'\nМоя кличка: {self.name}!'
+                         f'\nМой пол: {self.sex}'
+                         f'\nЯ {self.__get_was_born()} '
+                         f'{self.date_of_birth}г.')
+        print(template)
+        # return template
 
 
+class Cat(AnimalBase):
+    def __init__(self, name, sex, date_of_birth):
+        super().__init__(name, sex, date_of_birth)
 
-barsik = Animal('Барсик', 10, 'мужской', "01.01.1998")
+
+class Dog(AnimalBase):
+    def __init__(self, name, sex, date_of_birth):
+        super().__init__(name, sex, date_of_birth)
+
+
+class Lion(AnimalBase):
+    def __init__(self, name, sex, date_of_birth):
+        super().__init__(name, sex, date_of_birth)
+
+
+# ----------------------------------------
+
+barsik = Cat('Барсик', 'мужской', "01.01.1998")
 barsik.get_info()
 
-murzik = Animal('Мурзик', 11, 'мужской', '02.03.1997')
-murzik.get_info()
+murzik = Cat('Мурзик', 'мужской', '02.03.1997')
+# murzik.get_info()
 
-animal_snezhok = Animal('Снежок', 7, 'мужской', '03.05.2001')
-animal_snezhok.get_info()
+animal_snezhok = Cat('Снежок', 'мужской', '03.05.2001')
+# animal_snezhok.get_info()
 
-animal_snezhana = Animal('Снежана', 6, 'женский', '04.06.2002')
+animal_snezhana = Cat('Снежана', 'женский', '04.06.2002')
 animal_snezhana.get_info()
+
+# ----------------------------------------
+
+# Dog
+alabay = Dog('Алабай', 'мужской', '01.01.1970')
+doberman = Dog('Доберман', 'мужской', '01.01.1970')
+ejevicka = Dog('Ежевичка', 'женский', '01.01.1970')
+
+alabay.get_info()
+ejevicka.get_info()
+
+
+# Нет необходимости получать параметр age: это можно легко рассчитать,
+# зная год рождения. Текущий_год — Год_рождения = возраст
